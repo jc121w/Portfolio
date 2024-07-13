9e2e87d6a811db788139da3e36d8deed52f3d518
Jason Chen <jchen146@umd.edu>
2024-07-12 21:18:35 -0400

    testing img

--- src/hero/Hero.jsx
+++ src/hero/Hero.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "./hero.css";
 import { motion } from "framer-motion";
-
+import heroImg from "../../assets/GradPic.jpg";
 const Hero = ({ animationControl }) => {
   return (
     <motion.div className="hero_section" animate={animationControl}>
       <div className="hero_content">
         <motion.div className="clip" whileHover={{ scale: 1.05 }}>
-          <img
-            className="photo"
-            src="../assets/GradPic.jpg"
-            alt="hero_photo.jpg"
-          />
+          <img className="photo" src={heroImg} alt="hero_photo.jpg" />
         </motion.div>
         <div className="hero_text">
           <p className="hello">Hi, I'm Jason. </p>
