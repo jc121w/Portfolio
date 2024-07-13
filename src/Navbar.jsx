b73507f1acdfa1fec634e726ff3906062aa35383
Jason Chen <jchen146@umd.edu>
2024-07-12 22:24:47 -0400

    removed transcript

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,5 +1,5 @@
 import Resume from "../assets/Resume.pdf";
-import Transcript from "../assets/Transcript.pdf";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
 function Navbar() {
@@ -9,14 +9,7 @@ function Navbar() {
         <a href={Resume} className="link_item" target="_blank" rel="noreferrer">
           Resume
         </a>
-        <a
-          href={Transcript}
-          className="link_item"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Transcript
-        </a>
+
         <a
           href="https://www.linkedin.com/in/jchen2002/"
           className="link_item"
