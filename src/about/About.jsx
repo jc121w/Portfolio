b107330abafb3e2b9d80c13361b555e33adc471c
Jason Chen <jchen146@umd.edu>
2024-08-11 20:04:28 -0400

    Updated about me message

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -34,14 +34,26 @@ const About = ({ animationControl }) => {
       <motion.div className="about_text" variants={aboutComponentVariants}>
         <div className="title">About Me</div>
         <span>
-          I am an aspiring software engineer with a passion for building
-          innovative and impactful solutions. I am eager to work in
-          collaborative environments, where I can contribute and interact with
-          team members and learn from seasoned professionals. Beyond coding, I
-          enjoy fitness and competitive sports. Tennis, volleyball, and table
-          tennis are some of my favorite. I am excited to bring my creativity,
-          technical expertise, and dedication to a forward-thinking
-          organization, where I can grow and make an impact.
+          <p>
+            Hello, my name is Jason. I am a Spring 2024 new grad from the
+            University of Maryland College Park with a BS in Computer Science.
+          </p>
+          <p>
+            When I'm not coding, I enjoy a wide range of hobbies. Doing
+            calisthenics & weightlifting, playing tennis & volleyball, and
+            spending time with friends & family are some of my favorite past
+            times. I'm always eager to take part in competitve and collaborative
+            sports.
+          </p>
+          These hobbies translate into my coding. I love to learn new
+          technologies and work collaboratively alongside like-minded
+          individuals. Creating meaningful software and solving complex problems
+          whilst doing so excites me and is how I work my best.{" "}
+          <p>
+            I am open to new opportunities. Feel free to shoot me a message if
+            you want to talk. I enjoy meeting new people. Thanks for checking
+            out my porfolio!
+          </p>
         </span>
       </motion.div>
     </motion.div>
